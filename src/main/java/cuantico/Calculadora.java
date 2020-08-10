448423c790194c370296c19e1f439834e9474b8b
andres <andres.rodriguez-de@mail.escuelaing.edu.co>
2020-08-10 18:06:25 -0500

    test subidos

--- src/main/java/cuantico/Calculadora.java
+++ src/main/java/cuantico/Calculadora.java
@@ -12,45 +12,45 @@ import java.text.DecimalFormat;
  * @author rodri
  */
 public class Calculadora {
-    public  NumeroComplejo suma(NumeroComplejo c1, NumeroComplejo c2) {
+    public  static NumeroComplejo suma(NumeroComplejo c1, NumeroComplejo c2) {
         return new NumeroComplejo(c1.getpReal() + c2.getpReal(), c1.getpImg() + c2.getpImg());
     }
-    public  NumeroComplejo producto(NumeroComplejo c1, NumeroComplejo c2) {
+    public static NumeroComplejo producto(NumeroComplejo c1, NumeroComplejo c2) {
         double pReal = (c1.getpReal() * c2.getpReal()) - (c1.getpImg() * c2.getpImg());
         double pImg = (c1.getpReal() * c2.getpImg()) + (c2.getpReal() * c1.getpImg());
         return new NumeroComplejo(pReal, pImg);
     }
-    public  NumeroComplejo resta(NumeroComplejo c1, NumeroComplejo c2) {
+    public  static NumeroComplejo resta(NumeroComplejo c1, NumeroComplejo c2) {
         return new NumeroComplejo(c1.getpReal() - c2.getpReal(), c1.getpImg() - c2.getpImg());
     }
-    public  NumeroComplejo division(NumeroComplejo c1, NumeroComplejo c2) {
+    public  static NumeroComplejo division(NumeroComplejo c1, NumeroComplejo c2) {
         double realNumerador = (c1.getpReal() * c2.getpReal()) + (c1.getpImg() * c2.getpImg());
         double imgNumerador = (c2.getpReal() * c1.getpImg()) - (c1.getpReal() * c2.getpImg());
         double denominador = Math.pow(c2.getpReal(), 2) + Math.pow(c2.getpImg(), 2);
         return new NumeroComplejo(Math.round((realNumerador / denominador) * 100.0) / 100.0, Math.round((imgNumerador / denominador) * 100.0) / 100.0);
     }
-     public double modulo(NumeroComplejo a) {
+     public static double modulo(NumeroComplejo a) {
         double real = Math.sqrt(Math.pow(a.getpReal(), 2) + Math.pow(a.getpImg(), 2));
         return real;
     }
-    public NumeroComplejo conjugado(NumeroComplejo a) {
+    public static NumeroComplejo conjugado(NumeroComplejo a) {
         NumeroComplejo nc = new NumeroComplejo(a.getpReal(),-a.getpImg());
         return nc;
     }
-    public NumeroPolar cartesianaAPolar(NumeroComplejo a){
+    public static NumeroPolar cartesianaAPolar(NumeroComplejo a){
         double radio = modulo(a);
         double rads = Math.atan(a.getpImg()/a.getpReal());
         NumeroPolar np= new NumeroPolar(Math.toDegrees(rads),radio); 
         return np;
     }
     
-    public  NumeroComplejo polarACartesiana(NumeroPolar a){
-        double radio = a.getRadio();
-        double radian = Math.toRadians(a.getAngulo()); 
-        NumeroComplejo nc= new NumeroComplejo(radio*Math.cos(radian),radio*Math.sin(radian));
-        return nc;
+    public static  NumeroComplejo polarACartesiana(double p, double t){
+         double a = p * Math.cos(t);
+        double b = p * Math.sin(t);
+        return new  NumeroComplejo(Math.round(a * 100.0) / 100.0, Math.round(b * 100.0) / 100.0);
+
     }
-     public double fase(NumeroComplejo a){
+     public static double fase(NumeroComplejo a){
         double rads = Math.atan(a.getpImg()/a.getpReal());
         double angulo = Math.toDegrees(rads);
         return angulo;
@@ -67,7 +67,6 @@ public class Calculadora {
         System.out.println("Modulo :" + app.modulo(a));
         System.out.println("Conjugado :" + app.conjugado(a));
         System.out.println("Cartesiano a Polar :" + app.cartesianaAPolar(a));
-        System.out.println("Polar a Cartesiano :" + app.polarACartesiana(c));
         System.out.println("Fase :" + app.fase(a));
     }
 }
--- src/test/java/cuantico/AppTest.java
+++ src/test/java/cuantico/AppTest.java
@@ -1,5 +1,6 @@
 package cuantico;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -13,8 +14,78 @@ public class AppTest
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
+    public void sumaNumComplejos()
     {
-        assertTrue( true );
+       NumeroComplejo c1= new NumeroComplejo(3,-1);
+       NumeroComplejo c2 = new NumeroComplejo(1, 4);
+       NumeroComplejo result = Calculadora.suma(c1, c2);
+       NumeroComplejo expected = new NumeroComplejo(4, 3);
+       assertEquals(expected, result);
+    }
+    @Test
+    public void ProductoNumComplejos()
+    {
+        NumeroComplejo c1 = new NumeroComplejo(3, -2);
+        NumeroComplejo c2 = new NumeroComplejo(1, 2);
+        NumeroComplejo result = Calculadora.producto(c1, c2);
+        NumeroComplejo expected = new NumeroComplejo(7, 4);
+        assertEquals(expected, result);
+    }
+    @Test
+    public void restaNumComplejos()
+    {
+        NumeroComplejo c1 = new NumeroComplejo(5, 7);
+        NumeroComplejo c2 = new NumeroComplejo(3, 1);
+        NumeroComplejo result = Calculadora.resta(c1, c2);
+        NumeroComplejo expected = new NumeroComplejo(2, 6);
+        assertEquals(expected, result);
+    }
+    @Test
+    public void DivisionNumComplejos()
+    {
+        NumeroComplejo c1 = new NumeroComplejo(-2, 1);
+        NumeroComplejo c2 = new NumeroComplejo(1, 2);
+        NumeroComplejo result = Calculadora.division(c1, c2);
+        NumeroComplejo expected = new NumeroComplejo(0, 1);
+        assertEquals(expected, result);
+    }
+    @Test
+    public void ModuloNumComplejos()
+    {
+        NumeroComplejo c1 = new NumeroComplejo(1, -1);
+        double expected = Math.sqrt(2);
+        double result = c1.modulo();
+        assertEquals(expected, result, 0);
+    }
+    @Test
+    public void ConjugadoNumComplejos()
+    {
+       NumeroComplejo c1 = new NumeroComplejo(2, 5);
+       NumeroComplejo expected = new NumeroComplejo(2, -5);
+       NumeroComplejo result = c1.conjugado();
+       assertEquals(expected, result);
+    }
+    @Test
+    public void polarCartesiano()
+    {
+        NumeroComplejo result = Calculadora.polarACartesiana(Math.sqrt(2), Math.PI / 4.0);
+        NumeroComplejo expected = new NumeroComplejo(1, 1);
+        assertEquals(expected, result);
+    }
+    @Test
+    public void CartesianoPolar()
+    {
+        NumeroComplejo c1 = new NumeroComplejo(1, 1);
+        String expected = "(1,41, 0,79)";
+        NumeroPolar result = Calculadora.cartesianaAPolar(c1);
+        assertEquals(expected, result);
+    }
+    @Test
+    public void Fase()
+    {
+        NumeroComplejo c1 = new NumeroComplejo(1, 1);
+        double expected = Math.PI / 4;
+        double result = c1.getFase();
+        assertEquals(expected, result, 0);
     }
 }
